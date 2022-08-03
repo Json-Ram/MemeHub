76ed4c96502396ae52ca00e28e70b4cae8ee4b5e
JRam2022 <jasonram778@gmail.com>
2022-08-03 15:24:30 -0700

    Add routes

--- server/index.js
+++ server/index.js
@@ -1,6 +1,11 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRoutes from "./routes/users.js";
+import videoRoutes from "./routes/videos.js";
+import commentRoutes from "./routes/comments.js";
+import authRoutes from "./routes/auth.js";
+
 
 const app = express()
 dotenv.config();
@@ -13,7 +18,12 @@ const connect = () => {
   .catch((err) => {
     throw err;
   });
-}
+};
+
+app.use("/api/auth", authRoutes)
+app.use("/api/users", userRoutes)
+app.use("/api/videos", videoRoutes)
+app.use("/api/comments", commentRoutes)
 
 
 app.listen(8800, () => {
