408bd87f8540a7a1c6874cf9bc94a5430a117415
JRam2022 <jasonram778@gmail.com>
2022-08-04 20:56:55 -0700

    Add verifyToken to update route

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,8 +1,28 @@
 import express from "express";
-import { } from "../controllers/user.js";
+import { dislike, getUser, like, remove, subscribe, unsubscribe, update } from "../controllers/user.js";
+import { verifyToken } from "../verifyToken.js";
 
 const router = express.Router();
 
+// Update user
+router.put("/:id", verifyToken, update);
 
+// Delete user
+router.delete("/:id", remove)
+
+// Get user
+router.get("/find/:id", getUser)
+
+// Subscribe user
+router.put("/subscribe/:id", subscribe)
+
+// Unsubscribe user
+router.put("/unsubscribe/:id", unsubscribe)
+
+// Like video
+router.put("/like/:videoId", like)
+
+// Dislike video
+router.put("/dislike/:videoId", dislike)
 
 export default router;
