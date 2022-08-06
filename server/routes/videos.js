e01d3bf40f86f061723e01685a5f352ec84474cf
JRam2022 <jasonram778@gmail.com>
2022-08-06 00:33:40 -0700

    Add routes for views, trending, random and subscribed

--- server/routes/videos.js
+++ server/routes/videos.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { addVideo, deleteVideo, getVideo, updateVideo } from "../controllers/video.js";
+import { addVideo, addView, deleteVideo, getVideo, randomVideo, subscribedVideo, trendingVideo, updateVideo } from "../controllers/video.js";
 import { verifyToken } from "../verifyToken.js";
 
 const router = express.Router();
@@ -17,16 +17,16 @@ router.delete("/:id", verifyToken, deleteVideo);
 router.get("/find/:id", getVideo);
 
 // Views of video
-router.put("/views/:id", getVideo);
+router.put("/views/:id", addView);
 
 // Trending videos
-router.get("/trending", getVideo);
+router.get("/trending", trendingVideo);
 
 // Random videos
-router.get("/random", getVideo);
+router.get("/random", randomVideo);
 
 // Subcribed to videos
-router.get("/subscribed", getVideo);
+router.get("/subscribed", verifyToken, subscribedVideo);
 
 
 export default router;
