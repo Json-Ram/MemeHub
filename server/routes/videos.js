d065c7fb4c36121fe03835f08bb447c42a94d23e
JRam2022 <jasonram778@gmail.com>
2022-08-05 22:38:01 -0700

    Create routes for Create, Update, Delete, Find video

--- server/routes/videos.js
+++ server/routes/videos.js
@@ -1,8 +1,20 @@
 import express from "express";
-import { } from "../controllers/video.js";
+import { addVideo, deleteVideo, getVideo, updateVideo } from "../controllers/video.js";
+import { verifyToken } from "../verifyToken.js";
 
 const router = express.Router();
 
+// Create video
+router.post("/", verifyToken, addVideo);
+
+// Update video
+router.put("/:id", verifyToken, updateVideo);
+
+// Delete video
+router.delete("/:id", verifyToken, deleteVideo);
+
+// Find video
+router.get("/find/:id", getVideo);
 
 
 export default router;
