b0f08344958276cf5c27c778037c16c1752c7d75
JRam2022 <jasonram778@gmail.com>
2022-08-05 22:31:50 -0700

    Create subscribe and unsubscribe function in User Controller

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -38,25 +38,41 @@ export const remove = async (req, res, next) => {
 
 export const getUser = async (req, res, next) => {
   try {
-
+    const user = await User.findById(req.params.id);
+    res.status(200).json(user)
   }catch(err) {
     next(err)
   }
 }
+
 export const subscribe = async (req, res, next) => {
   try {
-
+    await User.findById(req.params.id, {
+      $push: { subscribed:req.params.id }
+    });
+    await User.findByIdAndUpdate(req.params.id, {
+      $inc: { subscribers: 1 }
+    });
+    res.status(200).json("Subscribed")
   }catch(err) {
     next(err)
   }
 }
+
 export const unsubscribe = async (req, res, next) => {
   try {
-
+    await User.findById(req.user.id, {
+      $pull: { subscribed:req.params.id }
+    });
+    await User.findByIdAndUpdate(req.params.id, {
+      $inc: { subscribers: -1 }
+    });
+    res.status(200).json("Unsubscribed")
   }catch(err) {
     next(err)
   }
 }
+
 export const like = async(req, res, next) => {
   try {
 
