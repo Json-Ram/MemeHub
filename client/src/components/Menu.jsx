696e5ecbe03e3d7ae105982c662814040152abef
JRam2022 <jasonram778@gmail.com>
2022-08-08 22:02:06 -0700

    Create, Style  Navbar and Adjust Menu style

--- client/src/components/Menu.jsx
+++ client/src/components/Menu.jsx
@@ -15,7 +15,7 @@ import LightModeIcon from '@mui/icons-material/LightMode';
 
 const Container = styled.div`
   flex: 1;
-  background-color: ${({theme}) =>theme.bg};
+  background-color: ${({theme}) =>theme.bgLighter};
   height: 100vh;
   color: ${({theme}) =>theme.text};
   font-size: 12px;
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,8 +1,73 @@
 import React from 'react'
+import styled from 'styled-components';
+import FaceIcon from '@mui/icons-material/Face';
+import LocationSearchingIcon from '@mui/icons-material/LocationSearching';
+
+
+const Container = styled.div`
+  position: sticky;
+  top: 0;
+  background-color: ${({ theme }) => theme.bgLighter}
+  height: 56px;
+`;
+
+const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+  height: 100%;
+  padding: 0px 20px;
+  position: relative;
+`;
+
+const Search = styled.div`
+  width: 40%;
+  position: absolute;
+  left: 0;
+  right: 0;
+  margin: auto;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 5px;
+  border: 1px solid grey;
+  border-radius: 3px;
+`;
+
+const Input = styled.input`
+  border: none;
+  background-color: transparent;
+
+`;
+
+const Button = styled.button`
+  padding: 5px 15px;
+  background-color: transparent;
+  border: 1px solid green;
+  color: green;
+  border-radius: 3px;
+  font-weight: 500;
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  gap: 5px;
+`;
+
 
 function Navbar() {
   return (
-    <div>Navbar</div>
+    <Container>
+      <Wrapper>
+        <Search>
+          <Input placeholder="Search" />
+          <LocationSearchingIcon />
+        </Search>
+        <Button>
+          <FaceIcon />
+          Sign In
+        </Button>
+      </Wrapper>
+    </Container>
   )
 }
 
