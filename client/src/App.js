8c7bca5e37e3270850a1ad9b8e34b7de825125cb
JRam2022 <jasonram778@gmail.com>
2022-08-09 22:42:28 -0700

    Quick fix

--- client/src/App.js
+++ client/src/App.js
@@ -31,9 +31,9 @@ function App() {
         <Wrapper>
           <Routes>
             <Route path="/">
-              <Route index element={Home} />
+              <Route index element={<Home/>} />
               <Route path="video">
-                <Route path=":id" element={Video} />
+                <Route path=":id" element={<Video/>} />
               </Route>
             </Route>
           </Routes>
