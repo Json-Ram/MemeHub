a08bcb3f88ec74646fa3197272cd1f48b6a98126
JRam2022 <jasonram778@gmail.com>
2022-08-11 23:55:15 -0700

    Add base style to Video page

--- client/src/pages/Video.jsx
+++ client/src/pages/Video.jsx
@@ -10,19 +10,31 @@ const Content = styled.div`
   flex: 5;
 `;
 
-const videoWrapper = styled.div`
+const VideoWrapper = styled.div`
 
 `;
 
 const Title = styled.div`
-  
+  font-size: 18px;
+  font-weight: 400;
+  margin-top: 20px;
+  margin-bottom: 10px;
+  color: ${({ theme }) => theme.text};
 `;
 
 const Details = styled.div`
-  
+  diplay: flex;
+  align-items: center;
+  justify-content: space-between;
 `;
 
 const Info = styled.span`
+  color: ${({ theme }) => theme.textSoft};
+`;
+
+const Buttons = styled.div``;
+
+const Button = styled.div`
   
 `;
 
@@ -34,8 +46,32 @@ const Recommended = styled.div`
 const Video = () => {
   return (
     <Container>
-      <Content>content</Content>
-      <Recommended>recommendations</Recommended>
+      <Content>
+        <VideoWrapper>
+        <iframe 
+          width="1000" 
+          height="500" 
+          src="https://www.youtube.com/embed/qsfISQIZt9A?list=PLSzdkW8i2By_7DRe18oyQDecbc-k7X2Mt" 
+          title="Scooby Doo Where Are You! 1 4   Hassle In The Castle" 
+          frameborder="0" 
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+          allowfullscreen
+        ></iframe>
+        </VideoWrapper>
+        <Title>Scooby Doo Season Episode 1</Title>
+        <Details>
+          <Info> 7,045,349 views ~ Aug 11, 2022</Info>
+          <Buttons>
+            <Button>button</Button>
+            <Button>button</Button>
+            <Button>button</Button>
+            <Button>button</Button>
+          </Buttons>
+        </Details>
+      </Content>
+      <Recommended>
+        recommendations
+      </Recommended>
     </Container>
   )
 }
